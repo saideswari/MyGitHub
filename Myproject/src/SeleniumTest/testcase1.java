c3a625eff9a78e246a72ead87cf3b805831ee7b9
prave <prave@DESKTOP-SC9G4M3>
2021-09-30 14:41:27 -0700

    commit message

--- Myproject/src/SeleniumTest/testcase1.java
+++ Myproject/src/SeleniumTest/testcase1.java
@@ -5,6 +5,7 @@ public class testcase1 {
 	public static void main(String[] args) {
 		System.out.println("this is my first testcase");
 		System.out.println("testcase modified");
+		System.out.println("hello");
 
 	}
 
